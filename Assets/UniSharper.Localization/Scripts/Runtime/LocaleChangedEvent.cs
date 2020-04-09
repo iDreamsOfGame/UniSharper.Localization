abf9d5a299ee666605abe1062298721967c1473f
Jerry Lee <cosmos53076@163.com>
2020-04-10 00:14:06 +0800

    Initialize unity project.

---
+++ Assets/UniSharper.Localization/Scripts/Runtime/LocaleChangedEvent.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Jerry Lee. All rights reserved. Licensed under the MIT License. See LICENSE in the
+// project root for license information.
+
+using System;
+
+namespace UniSharper.Localization
+{
+    /// <summary>
+    /// Contains the event data of the LocaleChanged event. Implements the <see cref="System.EventArgs"/>
+    /// </summary>
+    /// <seealso cref="System.EventArgs"/>
+    public class LocaleChangedEventArgs : EventArgs
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocaleChangedEventArgs"/> class.
+        /// </summary>
+        /// <param name="currentLocale">The current locale.</param>
+        public LocaleChangedEventArgs(Locale currentLocale)
+            : base()
+        {
+            CurrentLocale = currentLocale;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the current locale.
+        /// </summary>
+        /// <value>The current locale.</value>
+        public Locale CurrentLocale
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
