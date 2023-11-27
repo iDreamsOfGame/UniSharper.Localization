b350841fce94d20846d61979bff96a1e21a4c703
Jerry Lee <cosmos53076@163.com>
2023-11-27 11:50:53 +0800

    Optimizes code.

--- Editor/LocalizationAssetUtility.cs
+++ Editor/LocalizationAssetUtility.cs
@@ -99,7 +99,7 @@ namespace UniSharperEditor.Localization
             }
 
             var dirPath = EditorPath.GetFullPath(settings.LocalizationAssetsPath);
-            var files = Directory.GetFiles(dirPath, SearchPattern.BinaryFiles);
+            var files = Directory.GetFiles(dirPath, SearchPattern.UnityBinaryFiles);
 
             if (files.Length <= 0)
                 return null;
